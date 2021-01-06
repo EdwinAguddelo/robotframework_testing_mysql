6962eb5d9c94ae38ee544ecc0e4445922138003c
Jeisson Andres Palacio Velasquez <yei-andres@hotmail.com>
2021-01-05 23:46:33 -0500

    secrets manager asumiendo rol

--- resources/access_secrets.py
+++ resources/access_secrets.py
@@ -25,24 +25,29 @@ def get_secret(secret_name_params):
 
     | Get Secret | ../access_secrets.py
     """
-    secret_name = secret_name_params
-    region_name = "us-east-1"
-    # Create a Secrets Manager client
-    
-    # https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_switch-role-api.html 
-
-    session = boto3.session.Session()
+    sts_client = boto3.client('sts')
+# Call the assume_role method of the STSConnection object and pass the role
+# ARN and a role session name.
+    assumed_role_object=sts_client.assume_role(
+        RoleArn="arn:aws:iam::874520149338:role/rol-acceso-a-s3-desde-ec2",
+        RoleSessionName="AssumeRoleSession1"
+    )
+# From the response that contains the assumed role, get the temporary 
+# credentials that can be used to make subsequent API calls
+    credentials=assumed_role_object['Credentials']
+    session = boto3.Session(
+        aws_access_key_id=credentials['AccessKeyId'],
+        aws_secret_access_key=credentials['SecretAccessKey'],
+        aws_session_token=credentials['SessionToken'],
+        )
 
     client = session.client(
         service_name='secretsmanager',
-        region_name=region_name
-        )
-    # In this sample we only handle the specific exceptions for the ‘GetSecretValue’ API.
-    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-    # We rethrow the exception by default.
+        region_name="us-east-1"
+    )
     try:
         get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
+            SecretId=secret_name_params
         )
     except ClientError as e:
         if e.response['Error']['Code'] == 'DecryptionFailureException':
@@ -56,11 +61,8 @@ def get_secret(secret_name_params):
         if 'SecretString' in get_secret_value_response:
             secret = get_secret_value_response['SecretString']
             resp_data = json.loads(secret)
-            logger.console(resp_data['user'])
-            logger.console(resp_data['password'])
             os.environ["USER"] = resp_data['user']
             os.environ["PASSWORD"] = resp_data['password']        
         else:
             decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
     # Your code goes here.
-    
