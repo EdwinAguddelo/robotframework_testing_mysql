86048fb45d6ffc64d1138164338e448e12679a1b
Jeisson Andres Palacio Velasquez <yei-andres@hotmail.com>
2021-01-14 10:51:28 -0500

    configuracion credenciales

--- resources/configure.py
+++ resources/configure.py
@@ -1,12 +1,16 @@
 from collections import namedtuple
+import yaml
 
 def get_variables(environment):
+    with open (r'.\resources\configsecurity.yaml') as yamlfile:
+        fruit = yaml.load(yamlfile,Loader=yaml.FullLoader)
+        DBConfig = namedtuple('DBCONFIG', 'host, user, pwd')
+    DBConfig = namedtuple('DBCONFIG', 'host, user, pwd')
+
+    db_config = {
+        "local": DBConfig(fruit['host'], fruit['usuario'], fruit['password'])
+    }
     return {
         "dbconfig": db_config[environment]
     }
-
-DBConfig = namedtuple('DBCONFIG', 'host, user, pwd')
-
-db_config = {
-    "local": DBConfig("DESKTOP-MMLHIU2", "datical", "admin")
-}
+        
